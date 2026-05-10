2716b75043d151b58f53c4b018d162628b0d920b
KyawtKyawtZinRose <maymyostore763@gmail.com>
2026-05-10 18:08:33 +0400

    connected to login

---
+++ assets/js/login.js
@@ -0,0 +1,49 @@
+console.log("login js connected");
+
+function login(event) {
+  event.preventDefault();
+
+  // get input values
+  const loginInput = document.getElementById("loginInput").value.trim();
+
+  const password = document.getElementById("loginPassword").value.trim();
+
+  // get users from localStorage
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  // find matching user
+  const matchedUser = users.find((user) => {
+    return (
+      (user.username === loginInput || user.email === loginInput) &&
+      user.password === password
+    );
+  });
+
+  // validation
+  if (!matchedUser) {
+    alert("Invalid username/email or password");
+    return;
+  }
+
+  // save current logged in user
+  localStorage.setItem("currentUser", JSON.stringify(matchedUser));
+
+  alert("Login successful");
+
+  // role redirect
+  if (matchedUser.role === "parent") {
+    window.location.href = "../parent-page/index.html";
+  } else if (matchedUser.role === "teacher") {
+    window.location.href = "../teacher-page/index.html";
+  } else if (matchedUser.role === "student") {
+    // boy student
+    if (matchedUser.gender === "boy") {
+      window.location.href = "../student-boy-page/index.html";
+    }
+
+    // girl student
+    else {
+      window.location.href = "../student-girl-page/index.html";
+    }
+  }
+}
--- assets/js/signup_bk.js
+++ assets/js/signup_bk.js
@@ -6,9 +6,42 @@ const inputFormElement = document.getElementById("signUpForm");
 const usernameElement = document.getElementById("username");
 const emailElement = document.getElementById("email");
 const passwordElement = document.getElementById("password");
+const role = document.getElementById("role").value;
 
+const togglePasswordBtn = document.getElementById("togglePassword");
+
+togglePasswordBtn.addEventListener("click", () => {
+  const eyeIcon = document.getElementById("eye-icon");
+
+  let passwordType = passwordElement.getAttribute("type");
+
+  if (passwordType === "password") {
+    passwordElement.setAttribute("type", "text");
+  } else {
+    passwordElement.setAttribute("type", "password");
+  }
+
+  eyeIcon.classList.toggle("bi-eye");
+});
+
+// submit event
 inputFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //  clear previous errors
+  errorElement.innerHTML = "";
+  errorElement.style.display = "none";
+
   let errorMessages = [];
+  const username = usernameElement.value.trim();
+
+  const email = emailElement.value.trim();
+
+  const password = passwordElement.value.trim();
+
+  const role = document.getElementById("role").value;
+
+  const gender = document.getElementById("gender").value;
 
   if (usernameElement.value === "") {
     errorMessages.push("Username is required");
@@ -26,8 +59,12 @@ inputFormElement.addEventListener("submit", (event) => {
     errorMessages.push("Password should be at least 6 characters!");
   }
 
-  if (passwordElement.value.lenght > 20) {
+  if (passwordElement.value.length > 20) {
     errorMessages.push("Password should be no more than 20 characters!");
+  }
+
+  if (role === "") {
+    errorMessages.push("Role is required");
   }
 
   if (errorMessages.length > 0) {
@@ -36,29 +73,55 @@ inputFormElement.addEventListener("submit", (event) => {
     console.log(errors, typeof errors);
     errorElement.innerHTML = errors;
     errorElement.style.display = "block";
-    event.preventDefault();
-  }
-
-  const togglePasswordBtn = document.getElementById("togglePassword");
-
-  togglePasswordBtn.addEventListener("click", () => {
-    const eyeIcon = document.getElementById("eye-icon");
-    console.log("click toggle password");
-    console.log(passwordElement.attributes);
-    let passwordType = passwordElement.getAttribute("type");
-
-    if (passwordType === "password") {
-      passwordElement.setAttribute("type", "text");
-    } else {
-      passwordElement.setAttribute("type", "password");
-    }
-    eyeIcon.classList.toggle("bi-eye");
-  });
-
-  if (role === "") {
-    alert("Please select a role");
     return;
   }
+
+  // get users array
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  // for checking duplicate email registration
+  const existingUser = users.find((user) => user.email === email);
+
+  if (existingUser) {
+    errorElement.innerHTML = "Email already registered";
+
+    errorElement.style.display = "block";
+
+    return;
+  }
+
+  // create user
+  const user = {
+    id: Date.now(),
+    username,
+    email,
+    password,
+    role,
+    gender: role === "student" ? gender : null,
+  };
+
+  // add new user
+  users.push(user);
+
+  // save
+  localStorage.setItem("users", JSON.stringify(users));
+
+  // success message
+  errorElement.classList.remove("alert-danger");
+
+  errorElement.classList.add("alert-success");
+
+  errorElement.innerHTML = "Signup successful! Redirecting to login page...";
+
+  errorElement.style.display = "block";
+
+  // reset all
+  inputFormElement.reset();
+  genderBox.classList.add("d-none");
+
+  // redirect after 3 seconds
+  setTimeout(() => {
+    window.location.href = "../login.html";
+  }, 3000);
 });
 
 const gender = document.getElementById("gender").value;
@@ -73,39 +136,3 @@ function handleRoleChange() {
     genderBox.classList.add("d-none");
   }
 }
-
-if (role === "parent") {
-  window.location.href = "parent.html";
-} else if (role === "teacher") {
-  window.location.href = "teacher.html";
-} else if (role === "student") {
-  if (theme.geder === "boy") {
-    window.location.href = "boy.html";
-  } else theme.geder === "girl";
-  {
-    window.location.href = "girl.html";
-  }
-}
-
-// creating user array
-const user = {
-  id: Date.now(),
-  username,
-  email,
-  password,
-  role,
-  gender: role === "student" ? gender : null,
-};
-
-const users = JSON.parse(localStorage.getItem("users")) || [];
-users.push(user);
-
-localStorage.setItem("users", JSON.stringify(users));
-
-localStorage.setItem(
-  "userTheme",
-  JSON.stringify({
-    role: role,
-    gender: role === "student" ? gender : null,
-  }),
-);
